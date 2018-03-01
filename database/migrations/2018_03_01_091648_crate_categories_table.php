f564d6c2e43ae2695a07b826f24ba1ef9bbf9ee6
Tomas <tl@premiato.org>
2018-03-01 10:44:35 +0100

    categories table created and colums added

--- database/migrations/2018_03_01_091648_crate_categories_table.php
+++ database/migrations/2018_03_01_091648_crate_categories_table.php
@@ -13,7 +13,11 @@ class CrateCategoriesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('categories', function ($table){
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
     }
 
     /**
